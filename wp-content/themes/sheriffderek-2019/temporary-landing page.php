317f6e3e95ceb853f6761a70106a497d28329f9c
Derek Wood <derek@sheriffderek.consulting>
2019-06-07 10:36:00 -0700

    Add Zdog logo

--- wp-content/themes/sheriffderek-2019/temporary-landing page.php
+++ wp-content/themes/sheriffderek-2019/temporary-landing page.php
@@ -5,6 +5,8 @@
 get_header(); ?>
 
 
+
+
 <section class='page-section section-name'>
 <div class='inner-column'>
 
@@ -13,6 +15,20 @@ get_header(); ?>
 
         <article class='content x'>
           <!-- <h1 class='page-title'><?php  // the_title(); ?></h1> -->
+
+          <aside class='heading'>
+            <figure class='logo'>
+              <canvas class='space' width='60' height='60'></canvas>
+            </figure>
+            
+            <div class='info'>
+              <h1>Sheriffderek Consulting</h1>
+              <div class='contact'>
+                <a href='tel:2133734808'>(213) 373-4808</a>
+              </div>
+            </div>
+          </aside>
+
 
           <?php the_content(); ?> 
           <!-- example... how would you put a date in here... besides javascript? -->
@@ -32,6 +48,72 @@ get_header(); ?>
 
       <?php endwhile; ?>
     <?php endif; ?>
+
+
+
+<script src='https://unpkg.com/zdog@1/dist/zdog.dist.js'></script>
+
+<script>
+  var TAU = Zdog.TAU;
+  var darkPink = '#f06';
+  var lightPink = '#fCC';
+
+  // var space = document.getElementById('space');
+
+  let isSpinning = true;
+
+  var logo = new Zdog.Illustration({
+    element: '.space',
+    rotate: {
+      x: 2,
+      y: 1.1,
+    },
+    // zoom: 1,
+    dragRotate: true,
+    // stop rotation when dragging starts
+    onDragStart: function() {
+      isSpinning = false;
+    },
+  });
+
+  ( function() {
+
+    var sphere = new Zdog.Anchor({
+      addTo: logo,
+    });
+
+    var halve = new Zdog.Hemisphere({
+      addTo: sphere,
+      diameter: 60,
+      scale: 1,
+      color: lightPink,
+      backface: darkPink, // stops the blinking
+      stroke: false,
+    });
+
+    halve.copy({
+      rotate: { y: TAU/2 },
+      color: darkPink,
+      backface: lightPink,
+    });
+
+    logo.updateRenderGraph();
+  })();
+
+
+  function animate() {
+    // rotate illo each frame
+    if (isSpinning) {
+      logo.rotate.x += 0.003;
+    }
+    logo.updateRenderGraph();
+    // animate next frame
+    requestAnimationFrame( animate );
+  }
+  // start animation
+  animate();
+</script>
+
 
 </div>
 </section>
